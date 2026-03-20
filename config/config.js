6a8f502134eabc0779867eb078e2ddb20c86e209
OlachiFaith <okereolachifaith258@gmail.com>
2026-03-20 13:22:46 +0100

    server and database setup

---
+++ config/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const DB_Name = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+
+
+
+
+
+module.exports ={
+  "development": {
+    "username": username,
+    "password": password,
+    "database": DB_Name,
+    "host": host,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 7070
+const sequelize = require('./database/database');
+const productRouter = require('./routes/productRouter')
+
+const app = express();
+
+app.use(express.json());
+
+app.use(productRouter)
+
+const DB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected successfully');
+        
+    } catch (error) {
+       console.log('Error connecting to Database: ', error.message);
+        
+    }
+}
+
+DB();
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+    
+})
